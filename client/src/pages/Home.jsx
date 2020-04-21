c2e2090d55d6c8807bd2f28b577de48c1bcab83a
Phonekham <phone.madridista@gmail.com>
2020-04-21 16:49:04 +0700

    Displaying Posts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import { Grid } from "semantic-ui-react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
-function Home() {
-  return <div>home</div>;
-}
+import PostCard from "../components/PostCard";
+
+const Home = () => {
+  const { loading, data } = useQuery(FET_POST_QUERY);
+
+  return (
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading Posts</h1>
+        ) : (
+          data.getPosts &&
+          data.getPosts.map((post) => (
+            <Grid.Column key={post.id}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
+  );
+};
+
+const FET_POST_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      commentCount
+      likes {
+        username
+      }
+      comments {
+        body
+        createdAt
+        id
+        username
+      }
+    }
+  }
+`;
 
 export default Home;
